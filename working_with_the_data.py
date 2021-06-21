db9e4982e0174f702393957fe81e209bc50a22d7
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-06-21 14:07:18 +0200

    Update working_with_the_data.py

--- working_with_the_data.py
+++ working_with_the_data.py
@@ -1,5 +1,9 @@
 import numpy as np # Needed for create_dataset to work
-from utils import find_name_file_and_label
+from scipy import signal as sig
+from scipy import stats as st
+import mne
+
+from utils import find_name_file_and_label # Path might be different
 
 def create_dataset(conditions, subject_start, subject_end, cfg, f_band, check_nperseg, bands, pli):
   """
@@ -97,3 +101,113 @@ def create_dataset(conditions, subject_start, subject_end, cfg, f_band, check_np
   y_all = np.reshape(y_list, [total_subjects * tot_epochs])
   scores_all = np.reshape(scores_list, [total_subjects * tot_epochs])
   return X_all, y_all, scores_all, id_all
+
+
+
+def scorEpochs_PLI(cfg, data, bands):
+    """
+    Function to select the best (most homogenoous) M/EEG epochs from a
+    resting-state recordings.
+
+    INPUT
+       cfg: dictionary with the following key-value pairs
+            freqRange    - list with the frequency range used to compute the PLI by ScorEpochs
+            fs           - integer representing sample frequency
+            windowL      - integer representing the window length (in seconds)
+
+       data: 2d array with the time-series (channels X time samples)
+       bands: dictionary of frequency bands of interest
+
+    OUTPUT
+
+       idx_best_ep: list of indexes sorted according to the best score (this list should be used for the selection of the
+                     best epochs)
+
+       epochs:       3d list of the data divided in equal length epochs of length windowL (epochs X channels X time samples)
+
+       score_x_ep:   array of score per epoch
+    """
+
+    X = filter_data(data, cfg["fs"], bands)                                  # Perform the filtering of the data
+    epochs, epoch_lenght = split_epoch(X, cfg["fs"], cfg["windowL"])         # Split the filtered data in epochs
+    X_PLI = np.array([PLI(np.transpose(epoch)) for epoch in epochs])         # Compute the PLI for each epoch
+    n_ch = len(data)
+    n_ep = len(epochs)
+    X_PLI_x_ch = np.zeros((n_ep, n_ch))
+    score_ch_x_ep = np.zeros((n_ch, n_ep))
+    for c in range(n_ch):
+        for e in range(n_ep):
+            X_PLI_x_ch[e] = X_PLI[e][c]
+        score_ch, p = st.spearmanr(X_PLI_x_ch, axis=1)          # Correlation between the PLI of the epochs within each channel
+        score_ch_x_ep[c][0:n_ep] += np.mean(score_ch, axis=1)   # Mean similarity score of an epoch with all the epochs for each channel
+    score_x_ep = np.mean(score_ch_x_ep, axis=0)                 # The score of each epoch is equal to the mean of the scores of all the channels in that epoch
+    idx_best_ep = np.argsort(score_x_ep)                        # Obtains of the indexes from the worst epoch to the best
+    idx_best_ep = idx_best_ep[::-1]                             # Reversing to obtain the descending order (from the best to the worst)
+    return idx_best_ep, epochs, score_x_ep
+  
+  def filter_data(raw_data, srate, bands):
+    """
+     Function that applies a filter to the frequencies.
+
+     INPUT
+       raw_data: 2d array with the time-series EEG data of size: number of channels X samples
+       srate: integer, sampling rate
+       bands: dictionary of frequency bands of interest
+
+     OUTPUT
+       filtered_data: 2d array with the filtered EEG data
+    """
+    for band in bands:
+        low, high = bands[band]
+        filtered_data = mne.filter.filter_data(raw_data, srate, low, high)
+    return filtered_data
+
+def split_epoch(X, srate, t_epoch_lenght, t_discard=0):
+    """
+    Function that divides the signal in epochs.
+    INPUT
+     X:  2d array with the time-series EEG data.  number of channels X samples
+     srate: integer, sampling rate
+     t_epoch_lenght: integer, lenght of the epoch (in seconds)
+     t_discard: integer, initial portion of the record to be deleted (to eliminate initial artifacts)
+
+    OUTPUT
+     epochs: list of the data divided in equal length epochs
+     epoch_lenght: integer, lenght in samples - number of samples (per channel) for each epoch
+    """
+
+    [n_channels, n_sample] = X.shape
+
+    i_0 = t_discard * srate
+
+    epoch_lenght = t_epoch_lenght * srate
+
+    n_epochs = round((n_sample - i_0 - 1) / epoch_lenght)
+
+    epochs = []
+    for i_epoch in range(n_epochs):
+        i_start = i_0 + i_epoch * epoch_lenght
+        i_stop = i_start + epoch_lenght
+        epoch = X[0:n_channels, i_start:i_stop]
+        epochs.append(epoch)
+
+    return epochs, epoch_lenght
+
+
+
+def PLI(epoch):
+    """
+    Function to compute the PLI.
+    INPUT
+      epoch: 2d array with the data in the epoch
+    OUTPUT
+      PLI: 2d symmetric matrix, with shape number of channels X number of channels, containing the computed PLI values
+    """
+    nLoc = np.shape(epoch)[-1]
+    PLI = np.zeros(shape=(nLoc, nLoc))
+    complex_sig = sig.hilbert(epoch)
+    for i in range(nLoc - 1):
+        for j in range(i + 1, nLoc):
+            PLI[i, j] = abs(np.mean(np.sign(np.angle(np.divide(complex_sig[:, i],complex_sig[:, j])))));
+            PLI[j, i] = PLI[i, j];
+    return PLI
